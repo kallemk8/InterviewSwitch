a0357b20b01edb2ed02c588f886faaccd057e29b
Karra, Umesh (uk179v) <uk179v@att.com>
2022-09-05 20:54:57 +0530

    changes for switch actions for good and cheap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 function App() {
+  const [fast, setfast] = useState(false);
+  const [good, setGood] = useState(false);
+  const [cheap, setCheap] = useState(false);
+  const resetGood =  (value)=> {
+    if(!value){
+      setfast(false)
+      setCheap(false)
+    }
+    
+  }
+  const resetfast =  (value)=> {
+    if(!value){
+      setGood(false)
+      setCheap(false)
+    }
+    
+  }
+  const resetcheap =  (value)=> {
+    if(!value){
+      setfast(false)
+      setGood(false)
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Form>
+      <FormGroup switch>
+        <Input type="switch" checked={good} role="switch"
+        onClick={() => {
+          setGood(!good);
+          resetGood(good);
+        }} />
+        <Label check>Good</Label>
+      </FormGroup>
+      <FormGroup switch>
+        <Input
+          type="switch"
+          checked={fast}
+          onClick={() => {
+            setfast(!fast);
+            resetfast(fast);
+          }}
+        />
+        <Label check>Fast</Label>
+      </FormGroup>
+      <FormGroup switch >
+        <Input type="switch" checked={cheap} onClick={() => { setCheap(!cheap); resetcheap(cheap); }} />
+        <Label check>Cheap</Label>
+      </FormGroup>
+      
+    </Form>
     </div>
   );
 }
